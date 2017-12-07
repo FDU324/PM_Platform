79ddc43561f7d135958b9a1cdb50c2452b4b937a
shi pu <pushi@blacklake.cn>
2017-12-07 09:46:41 +0800

    First commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+
+class Testing extends React.Component {
+    render() {
+        return (
+            <Text>Hello {this.props.name}!</Text>
+        );
+    }
+}
+
+
+export default class App extends React.Component {
+    render() {
+        const pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+                <Text>Hello</Text>
+                <Image source={pic} style={{width: 193, height: 110}}/>
+                <Testing name='hhhsdfa'/>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
